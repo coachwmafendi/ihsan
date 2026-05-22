82c3dd347eda1dbe2d9fb6d194688d2ef694cb7e
wmafendi <wmafendi@gmail.com>
2026-05-22 15:22:09 +0800

    explicitly disable dark mode on /app panel

--- app/Providers/Filament/AppPanelProvider.php
+++ app/Providers/Filament/AppPanelProvider.php
@@ -37,6 +37,7 @@ class AppPanelProvider extends PanelProvider
             ->globalSearch(position: GlobalSearchPosition::Sidebar)
             ->profile(EditProfile::class, isSimple: false)
             ->homeUrl(fn (): string => route('filament.app.pages.insights'))
+            ->darkMode(false)
             ->databaseNotifications()
             ->colors([
                 'primary' => Color::Teal,
