f423134bd9c1ce48ed2b7206fea15cd8959acffd
wmafendi <wmafendi@gmail.com>
2026-05-26 07:06:14 +0800

    feat(donation): register donor_fee_covered in model

--- app/Models/Donation.php
+++ app/Models/Donation.php
@@ -11,7 +11,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
-#[Fillable(['campaign_id', 'donor_id', 'subscription_id', 'stripe_payment_intent_id', 'stripe_charge_id', 'payment_method_brand', 'payment_method_type', 'donor_country', 'gross_amount', 'stripe_fee', 'processing_fee', 'net_amount', 'currency', 'base_currency', 'base_amount', 'status', 'type', 'donor_message', 'is_anonymous', 'utm_params', 'invoice_number'])]
+#[Fillable(['campaign_id', 'donor_id', 'subscription_id', 'stripe_payment_intent_id', 'stripe_charge_id', 'payment_method_brand', 'payment_method_type', 'donor_country', 'gross_amount', 'stripe_fee', 'donor_fee_covered', 'processing_fee', 'net_amount', 'currency', 'base_currency', 'base_amount', 'status', 'type', 'donor_message', 'is_anonymous', 'utm_params', 'invoice_number'])]
 class Donation extends Model
 {
     /** @use HasFactory<DonationFactory> */
@@ -52,6 +52,7 @@ class Donation extends Model
         return [
             'gross_amount' => 'decimal:2',
             'stripe_fee' => 'decimal:2',
+            'donor_fee_covered' => 'decimal:2',
             'processing_fee' => 'decimal:2',
             'net_amount' => 'decimal:2',
             'base_currency' => 'string',
