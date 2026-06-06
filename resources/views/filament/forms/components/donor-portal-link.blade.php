834f378be8bd2ee7d5dcc8f8700be57d35401f0f
wmafendi <wmafendi@gmail.com>
2026-06-06 12:45:54 +0800

    feat: show Link sudah dicopy feedback on copy button in donor portal section

--- resources/views/filament/forms/components/donor-portal-link.blade.php
+++ resources/views/filament/forms/components/donor-portal-link.blade.php
@@ -10,13 +10,14 @@ $portalUrl = $org ? route('donorportal.dashboard', $org) : null;
         </div>
         <button
             type="button"
-            x-data
-            @click="navigator.clipboard.writeText('{{ $portalUrl }}'); $dispatch('notify', { message: 'Copied!' })"
+            x-data="{ copied: false }"
+            @click="navigator.clipboard.writeText('{{ $portalUrl }}'); copied = true; setTimeout(() => copied = false, 2000); $dispatch('notify', { message: 'Copied!' })"
             class="shrink-0 inline-flex items-center gap-1.5 rounded-lg border border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-800 px-3 py-2 text-sm font-medium text-gray-700 dark:text-gray-300 hover:bg-gray-50 dark:hover:bg-gray-700 transition-colors"
             title="Copy link"
         >
-            <x-heroicon-o-clipboard-document class="size-4" />
-            Copy
+            <x-heroicon-o-clipboard-document x-show="!copied" class="size-4" />
+            <x-heroicon-o-check x-show="copied" x-cloak class="size-4 text-green-600" />
+            <span x-text="copied ? 'Link sudah dicopy' : 'Copy'"></span>
         </button>
         <a
             href="{{ $portalUrl }}"
