b6b73fc6dca0432d619994127e939f8be10d065b
wmafendi <wmafendi@gmail.com>
2026-06-06 20:45:31 +0800

    fix: replace wrong sparkle SVG with proper copy icon and use plain JS onclick

--- app/Filament/App/Resources/Donors/Pages/EditDonor.php
+++ app/Filament/App/Resources/Donors/Pages/EditDonor.php
@@ -51,38 +51,24 @@ class EditDonor extends EditRecord
                 <h1 class="text-xl font-bold tracking-tight text-gray-950 dark:text-white">
                     {$donor->name}
                 </h1>
-                <div class="flex items-center gap-1 text-sm text-gray-500 dark:text-gray-400">
+                <div class="flex items-center gap-1.5 text-sm text-gray-500 dark:text-gray-400 flex-wrap">
                     <span class="font-medium text-gray-700 dark:text-gray-300">ID</span>
                     <span>{$publicId}</span>
                     <button
                         type="button"
-                        x-data="{ copied: false }"
-                        @click="
+                        onclick="
                             navigator.clipboard.writeText('{$publicId}');
-                            copied = true;
-                            setTimeout(() => copied = false, 2000);
+                            const btn = this;
+                            const original = btn.innerHTML;
+                            btn.innerHTML = '<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 20 20\" fill=\"currentColor\" class=\"w-3.5 h-3.5 text-green-500\"><path fill-rule=\"evenodd\" d=\"M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z\" clip-rule=\"evenodd\" /></svg>';
+                            setTimeout(() => btn.innerHTML = original, 2000);
                         "
-                        class="inline-flex items-center justify-center p-0.5 rounded-md hover:bg-gray-100 dark:hover:bg-gray-800 transition-colors"
+                        class="inline-flex items-center justify-center p-0.5 rounded-md hover:bg-gray-100 dark:hover:bg-gray-800 transition-colors cursor-pointer"
                         title="Copy ID"
                     >
-                        <svg
-                            x-show="!copied"
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                            class="w-3.5 h-3.5 text-gray-400"
-                        >
-                            <path fill-rule="evenodd" d="M13.887 3.182c.396.04.734.308.88.68l.324.853.98-.37a.75.75 0 01.963.748l-.148.876.79.487a.75.75 0 01.382.858l-.27 1.042.54.784a.75.75 0 01-.382 1.212l-1.042.27.487.79a.75.75 0 01-.748.963l-.876-.148-.37.98a.75.75 0 01-1.283.043l-.59-.886-.886.59a.75.75 0 01-1.042-.27l-.487-.79-.79.487a.75.75 0 01-.858-.382l1.042-.27-.54-.784a.75.75 0 01.382-1.212l1.042-.27-.487-.79a.75.75 0 01.748-.963l.876.148.37-.98a.75.75 0 01.68-.88zm-7.364 7.364a.75.75 0 01-.827-1.233l3-2a.75.75 0 01.827 1.233l-3 2zm-1.5 2.5a.75.75 0 01-.827-1.233l4.5-3a.75.75 0 01.827 1.233l-4.5 3z" clip-rule="evenodd" />
-                        </svg>
-                        <svg
-                            x-show="copied"
-                            x-cloak
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                            class="w-3.5 h-3.5 text-green-500"
-                        >
-                            <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-3.5 h-3.5 text-gray-400">
+                            <path d="M7 3.5A1.5 1.5 0 018.5 2h3.879a1.5 1.5 0 011.06.44l3.122 3.12A1.5 1.5 0 0117 6.622V12.5a1.5 1.5 0 01-1.5 1.5h-1v-3.879a1.5 1.5 0 01-.44-1.06L9.62 7.5H8.5A1.5 1.5 0 017 6.5v-3zM5.5 7h1.75l1.75 1.75V12.5a1.5 1.5 0 01-1.5 1.5h-1A1.5 1.5 0 015 12.5v-4A1.5 1.5 0 015.5 7z" />
+                            <path d="M4.5 15.75a1.5 1.5 0 001.5 1.5h6a1.5 1.5 0 001.5-1.5V13h-1v2.75a.5.5 0 01-.5.5h-6a.5.5 0 01-.5-.5v-6a.5.5 0 01.5-.5H7v-1H5.5a1.5 1.5 0 00-1.5 1.5v6z" />
                         </svg>
                     </button>
                     <span class="mx-1">·</span>
