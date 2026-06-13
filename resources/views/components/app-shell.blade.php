cc7f4fb5e555e20327d15de74acfc40ad73c3896
wmafendi <wmafendi@gmail.com>
2026-06-13 18:08:48 +0800

    revert: restore original fixed sidebar layout

--- resources/views/components/app-shell.blade.php
+++ resources/views/components/app-shell.blade.php
@@ -3,7 +3,7 @@
     <x-sidebar />
     <div class="flex-1 flex flex-col min-w-0 lg:pl-64">
         <x-topbar />
-        <main class="flex-1 p-6 md:p-8 overflow-y-auto">
+        <main class="flex-1 p-6 md:p-8">
             {{ $slot }}
         </main>
     </div>
