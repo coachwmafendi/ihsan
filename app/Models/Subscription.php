6416bab36b211b211a47056429b7f5a8406a1d26
wmafendi <wmafendi@gmail.com>
2026-06-28 18:49:39 +0800

    Allow chip_recurring_token to be fillable on Subscription

--- app/Models/Subscription.php
+++ app/Models/Subscription.php
@@ -26,6 +26,7 @@ use Spatie\Activitylog\Support\LogOptions;
  * @property int|null $donor_payment_method_id
  * @property string|null $stripe_subscription_id
  * @property string|null $stripe_price_id
+ * @property string|null $chip_recurring_token
  * @property numeric $amount
  * @property string $currency
  * @property SubscriptionInterval $interval
@@ -90,7 +91,8 @@ use Spatie\Activitylog\Support\LogOptions;
  *
  * @mixin \Eloquent
  */
-#[Fillable(['campaign_id', 'donor_id', 'donor_payment_method_id', 'source', 'public_id', 'stripe_subscription_id', 'stripe_price_id', 'amount', 'currency', 'interval', 'status', 'retry_count', 'payment_count', 'failed_installment_count', 'cancel_at_period_end', 'cover_fee', 'fee_cover_amount', 'cancel_at', 'max_plan_amount', 'max_plan_installments', 'current_period_start', 'current_period_end', 'next_charge_at', 'last_charge_at', 'last_charge_attempt_at', 'paused_until', 'cancelled_at', 'cancellation_reason'])]
+#[Fillable(['campaign_id', 'donor_id', 'donor_payment_method_id', 'source', 'public_id', 'stripe_subscription_id', 'stripe_price_id', 'amount', 'currency', 'interval', 'status', 'retry_count', 'payment_count', 'failed_installment_count', 'cancel_at_period_end', 'cover_fee', 'fee_cover_amount', 'cancel_at', 'max_plan_amount', 'max_plan_installments', 'current_period_start', 'current_period_end', 'next_charge_at', 'last_charge_at', 'last_charge_attempt_at', 'paused_until', 'cancelled_at', 'cancellation_reason',
+    'chip_recurring_token'])]
 class Subscription extends Model
 {
     /** @use HasFactory<SubscriptionFactory> */
