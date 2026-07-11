adc74a8386f28a98d7bae62e3a19d25a24fb12fe
wmafendi <wmafendi@gmail.com>
2026-07-11 17:06:35 +0800

    fix(ui/tooltip): use sanitized tooltip text to prevent Livewire marker leakage in sidebar tooltips

--- resources/views/components/ui/tooltip.blade.php
+++ resources/views/components/ui/tooltip.blade.php
@@ -57,7 +57,7 @@
                 @if ($tipHtml)
                     {!! $tipHtml !!}
                 @else
-                    {{ $text }}
+                    {{ $tooltipText }}
                 @endif
             </div>
         </template>
